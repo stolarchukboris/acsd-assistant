e36549afd1d49be4d5e2b62119c45ce16a229727
stolarchukboris <kotkotovic07@gmail.com>
2025-12-27 19:34:23 +0300

    fix: Make it account for server shutdowns in case the game doesnt update all webhooks

--- src/events/messageUpdate.ts
+++ src/events/messageUpdate.ts
@@ -25,24 +25,53 @@ export async function execute(oldMessage: Message, newMessage: Message) {
         await fwMessage.edit({ embeds: newMessage.embeds });
 
         const timestampRegex = /\:([^:]+)\>/;
-        const fields = newMessage.embeds[0].fields;
-        const [started, ended] = [Number(fields[0].value.match(timestampRegex)![1]), Number(fields[1].value.match(timestampRegex)![1])];
-        const lengthMins = Math.round((ended - started) / 60);
 
-        await bot.knex<activeShift>('activeShifts')
-            .del()
-            .where('robloxId', activeShiftEntry.robloxId);
+        if (newMessage.embeds[0].title?.includes('(server shutdown)')) {
+            const unfinishedShifts = await bot.knex<activeShift>('activeShifts')
+                .select('*')
+                .where('jobId', activeShiftEntry.jobId);
 
-        await bot.knex<pendingShift>('pendingShiftLogs')
-            .insert({
-                jobId: activeShiftEntry.jobId,
-                robloxId: activeShiftEntry.robloxId,
-                whMessageId: activeShiftEntry.whMessageId,
-                fwMessageId: activeShiftEntry.fwMessageId,
-                startedTimestamp: String(started),
-                endedTimestamp: String(ended),
-                lenMinutes: lengthMins
-            });
+            for (const shift of unfinishedShifts) {
+                const message = await (bot.channels.cache.get(bot.env.SHIFT_LOGS_CH_ID) as TextChannel).messages.fetch(shift.whMessageId);
+                const fields = message.embeds[0].fields;
+                const [started, ended] = [Number(fields[0].value.match(timestampRegex)![1]), Number(fields[1].value.match(timestampRegex)![1])];
+                const lengthMins = Math.round((ended - started) / 60);
+
+                await bot.knex<activeShift>('activeShifts')
+                    .del()
+                    .where('robloxId', shift.robloxId);
+
+                await bot.knex<pendingShift>('pendingShiftLogs')
+                    .insert({
+                        jobId: shift.jobId,
+                        robloxId: shift.robloxId,
+                        whMessageId: shift.whMessageId,
+                        fwMessageId: shift.fwMessageId,
+                        startedTimestamp: String(started),
+                        endedTimestamp: String(ended),
+                        lenMinutes: lengthMins
+                    });
+            }
+        } else {
+            const fields = newMessage.embeds[0].fields;
+            const [started, ended] = [Number(fields[0].value.match(timestampRegex)![1]), Number(fields[1].value.match(timestampRegex)![1])];
+            const lengthMins = Math.round((ended - started) / 60);
+
+            await bot.knex<activeShift>('activeShifts')
+                .del()
+                .where('robloxId', activeShiftEntry.robloxId);
+
+            await bot.knex<pendingShift>('pendingShiftLogs')
+                .insert({
+                    jobId: activeShiftEntry.jobId,
+                    robloxId: activeShiftEntry.robloxId,
+                    whMessageId: activeShiftEntry.whMessageId,
+                    fwMessageId: activeShiftEntry.fwMessageId,
+                    startedTimestamp: String(started),
+                    endedTimestamp: String(ended),
+                    lenMinutes: lengthMins
+                });
+        }
 
         await newMessage.react('🟨');
     } catch (error) {
